e2f20ef72f9f99c3179fecc3762093feccb4b3f4
Pauchag <56460540+Pauchag@users.noreply.github.com>
2019-11-13 07:02:36 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Landing from "./pages/landing.js"
+
+function App() {
+  return (
+    <Landing/>
+  );
+}
+
+export default App;
